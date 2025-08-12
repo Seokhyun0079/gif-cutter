c2f25eb950a28adecab427f1524c3f26816f78da
Seokhyun0079 <seokhyun0324@gmail.com>
2025-08-12 22:46:52 +0900

    hosting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 상대 경로로 설정하여 GitHub Pages에서 작동하도록 함
 })
